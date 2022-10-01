572e9a823b85e3e99c5bfde520d8873a200a9391
Elyees <elytatua@gmail.com>
2022-10-01 22:43:44 +0300

    changed file format

---
+++ index.js
@@ -0,0 +1,16 @@
+/*const age = 19;
+const humanDogRatio = 7 * + "dogyears";
+console.log(age) */
+/*let name = "melissa"
+let notification = 'you have tree new notifications'; */
+
+/*console.log(notification);
+alert(name + "," + notification); */
+
+/*let messageToUser = name + ", " + notification;
+console.log(messageToUser); */
+
+/*let name = 'Elyees';
+let greeting = "hi my name is";
+let myGreeting = greeting + ", " + name + ".";
+console.log(myGreeting); */
---
+++ main.js
@@ -0,0 +1,16 @@
+let myPoints = 3;
+
+function add3Points() {
+    myPoints += 3;
+
+}
+
+function remove1Point(){
+    myPoints -= 1;
+};
+console.log(myPoints);
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
